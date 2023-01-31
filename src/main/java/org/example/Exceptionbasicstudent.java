6cf43fc6d73179b2150ed81f81824796416b44ae
subhi-29 <subhiksha.b@tringapps.com>
2023-01-31 14:44:37 +0530

    Initial commit

---
+++ src/main/java/org/example/Exceptionbasicstudent.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class MyException extends Exception{
+    public MyException(String str){
+        super(str);
+    }
+}
+class Student{
+    String stuname;
+    String grade;
+    double gpa;
+    double gradeValue;
+    double credit;
+    double mark;
+    Logger l = Logger.getLogger("com.api.jar");
+
+    Student(){
+        Scanner sc=new Scanner(System.in);
+
+        l.info("\nEnter Your Name:");
+        stuname=sc.nextLine();
+        try {
+
+
+            l.info("\nEnter the Grade level:");
+            grade = sc.nextLine();
+            if(grade.length()>2){
+                throw new MyException("Greater than string length");
+            }
+
+        }catch (MyException ex){
+            l.log(Level.INFO, () ->"\nException Occur:" +ex);
+            System.exit(0);
+        }
+
+
+
+        l.info("\nEnter the Credit points:");
+        credit=sc.nextDouble();
+    }
+    String showdetails()
+    {
+        l.log(Level.INFO, () ->"\nStudent Name:" +stuname);
+        l.log(Level.INFO, () ->"\nGrade Level:" +grade);
+        return stuname+" has a "+gpa+" GPA";
+    }
+    void checkgrade(){
+
+
+        switch (grade) {
+            case "A" -> gradeValue = 4.00;
+
+            case "A-"-> gradeValue=3.35;
+            case "B+" -> gradeValue = 3.00;
+            case "C" -> gradeValue = 2.67;
+            case "C+" -> gradeValue = 2.33;
+            case "D" -> gradeValue = 2.00;
+            default -> l.info("Invalid Grade");
+        }
+    }
+    void updategpa(){
+        mark=gradeValue*credit;
+        gpa=mark/credit;
+    }
+
+    public static void main(String[] args){
+        Logger l = Logger.getLogger("com.api.jar");
+        Student s=new Student();
+
+        s.checkgrade();
+        s.updategpa();
+        l.info(s.showdetails());
+
+    }
+}
---
+++ src/main/java/org/example/Exceptioncard.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Exceptioncard implements Cloneable{
+    String holdersname;
+    String cardnumber;
+    String expirationdate;
+
+    String name="swathi";
+    String number="1248 9087 3456 9087";
+    String date="12/24";
+    static Logger l=Logger.getLogger("com.api.jar");
+    Exceptioncard(){
+        l.info("The user can give the \n1,Card Holders name\n2,Card Number\n3, Expiration Date to create a creditcard");
+        l.info("\n Enter the Holders name,Card Number and Expiration Date :");
+        Scanner sc=new Scanner(System.in);
+        holdersname=sc.nextLine();
+        cardnumber=sc.nextLine();
+        expirationdate=sc.nextLine();
+        l.info("The Card holders name,Card number and Expiration Date will be created...");
+
+    }
+    String cardno(){
+        if(cardnumber.equals(number)){
+            return "true";
+        }
+        else{
+            return "false";
+        }
+    }
+    Exceptioncard cloneTest()
+    {
+        try {
+            return (Exceptioncard) super.clone();
+        }
+        catch(CloneNotSupportedException e)
+        {
+            l.info("Cloning not allowed");
+            return this;
+        }
+    }
+    public static void main(String[] args)throws CloneNotSupportedException{
+        Exceptioncard c1=new Exceptioncard();
+        l.log(Level.INFO, () ->"1,Same Card number--> returns True\n2,Different Card number--> returns False\n Answeris  "+c1.cardno());
+        Exceptioncard c2=c1.cloneTest();
+        l.log(Level.INFO, () ->"\nThe cloning method it returns Original Holders name "+c2. holdersname);
+        l.log(Level.INFO, () ->"\nThe cloning method it returns Original Card Number "+c2. cardnumber);
+        l.log(Level.INFO, () ->"\nThe cloning method it returns Original Expiration Date "+c2.expirationdate);
+
+    }
+}
